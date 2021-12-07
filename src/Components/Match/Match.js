ddd5c68ecafba8833287fe5050bea76eda272240
Shivanshu Gupta <shiv@Shivanshus-MBP.attlocal.net>
2021-12-06 23:19:30 -0800

    added backend compatability to past matches

--- src/Components/Match/Match.js
+++ src/Components/Match/Match.js
@@ -1,15 +1,28 @@
+import React from 'react';
 import './Match.css';
 
-function Match() {
-  return (
-    <div className="Match">
-        <div className="container">
-            <div className="date">Date</div>
-            <div className="time">Time</div>
-            <div className="wpm">WPM</div>
-        </div>
-    </div>
-  );
+class Match extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            date: this.props.matchData.date,
+            time: this.props.matchData.time,
+            wpm: this.props.matchData.wpm
+        }
+    }
+
+    render(){
+        const data = this.state;
+        return(
+          <div className="Match">
+              <div className="container">
+                  <div className="date">{data.date}</div>
+                  <div className="time">{data.time}</div>
+                  <div className="wpm">{data.wpm}</div>
+              </div>
+          </div>
+        )
+    }
 }
 
 export default Match;
--- src/Components/Matches/Matches.js
+++ src/Components/Matches/Matches.js
@@ -1,13 +1,47 @@
+import React from 'react';
 import './Matches.css';
 import Match from '../Match/Match';
 
-function Matches() {
-  return (
-    <div className="Matches">
-        <div className="title">Your Past Matches</div>
-        <Match />
-    </div>
-  );
-}
+class Matches extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    temp = [
+      { date: "11/29/2021",
+        time:"7:34 pm",
+        wpm:94
+      },
+      { date: "10/15/2021",
+        time:"6:09 pm",
+        wpm:101
+      },
+      { date: "12/6/2021",
+        time:"5:22 pm",
+        wpm:71
+      }
+    ]
+
+    render(){
+        //const data = this.state;
+
+        let display = []
+        for(let i = 0; i < Object.keys(this.temp).length;i++){
+          let data2 = this.temp[i]
+          display.push(
+                <Match key={i} matchData={data2}/>
+          )
+        }
+
+        return(
+          <div className="Matches">
+              <div className="title">Your Past Matches</div>
+              {display}
+          </div>
+        )
+    }
+  }
 
 export default Matches;
