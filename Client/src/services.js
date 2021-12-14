721e1bede50e0eddba5ac773b6892d9ff703c5cd
Shiv Gupta <sgupta35@ucmerced.edu>
2021-12-14 11:19:41 -0800

    socket io bug 2

--- Client/src/services.js
+++ Client/src/services.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 const port = process.env.PORT || 5050;
-const ENDPOINT = "localhost:" + port;
+const ENDPOINT = "https://server-domain.com" + port;
 
 export const socket = io(ENDPOINT);
